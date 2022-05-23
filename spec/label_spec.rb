cb93c504eb550b6e2f0632e3fa372c510aee3911
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-23 18:45:14 +0300

    add rest of properties for Label class

--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -5,5 +5,36 @@ describe Label do
     it 'is actually a class' do
       expect(Label).to be_a Class
     end
+
+    it 'gets initialized' do
+      title = 'abc'
+      color = 'red'
+
+      actual = Label.new(title, color)
+
+      expect(actual).to be_a Label
+    end
+
+    it 'sets properties as expected' do
+      title = 'abc'
+      color = 'red'
+      expected_static_properties = [
+        title,
+        color,
+        []
+      ]
+
+      the_label = Label.new(title, color)
+      actual_static_properties = [
+        the_label.title,
+        the_label.color,
+        the_label.items
+      ]
+      actual_id = the_label.id
+
+      expect(actual_static_properties).to eq expected_static_properties
+      expect(actual_id).to be_a Integer
+      expect(actual_id).to be > 0
+    end
   end
 end
