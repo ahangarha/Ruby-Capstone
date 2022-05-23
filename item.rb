aaab31a57710145553170dc1ee5a938601d8a98d
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-05-23 18:49:08 +0530

    Add item class

---
+++ item.rb
@@ -0,0 +1,20 @@
+class Item
+  attr_accessor :publish_date
+  attr_reader :archived
+
+  def initialize( id: rand(1..1000), publish_date, archived: false )
+    @id = id
+    @publish_date = publish_date
+    @archived = archived
+  end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  private
+  def can_be_archived?
+    current_year = 2022
+    current_year - publish_date > 10
+  end
+end
