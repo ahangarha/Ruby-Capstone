694b2c868a57bebbe421bad8779ad35f2bc4dac1
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-23 17:39:01 +0300

    set book properties

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -7,8 +7,31 @@ describe Book do
     end
 
     it 'is kind of Item class' do
-      actual = Book.new(1234)
+      publisher = 'abc'
+      cover_state = 'efg'
+      publish_date = 1234
+      actual = Book.new(publisher, cover_state, publish_date)
       expect(actual).to be_an Item
+    end
+
+    it 'sets properties properly' do
+      publisher = 'abc'
+      cover_state = 'efg'
+      publish_date = 1234
+      expected = [
+        publisher,
+        cover_state,
+        publish_date
+      ]
+
+      the_book = Book.new(publisher, cover_state, publish_date)
+      actual = [
+        the_book.publisher,
+        the_book.cover_state,
+        the_book.publish_date
+      ]
+
+      expect(actual).to eq expected
     end
   end
 end
