ed7c03e138cdf99bac45422bc0037655bfbc2b32
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-05-23 20:05:13 +0530

    Added game class

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,15 @@
 class Author
+  attr_accessor :first_name, :last_name
+  attr_reader :id, :items
+
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name = last_name
     @id = rand(1..1000)
     @items = []
+  end
+
+  def add_item(item)
+    @items << item unless @items.include?(item)
   end
 end
