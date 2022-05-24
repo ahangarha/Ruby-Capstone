7306cdc05297f4f20a1730d250c368d42c7fb2c3
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-24 21:25:18 +0300

    fix linter error

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -82,36 +82,26 @@ describe Book do
 
     it 'return correct hash for basic Book' do
       expected_items = {
-        class: 'Book',
-        publisher: publisher,
-        publish_date: publish_date,
-        cover_state: cover_state,
-        archived: false
+        class: 'Book', id: the_book.id, publisher: publisher,
+        publish_date: publish_date, cover_state: cover_state, archived: false
       }
-      expected_id = the_book.id
 
       hash = the_book.to_hash
-
       actual_items = {
-        class: hash['class'],
-        publisher: hash['publisher'],
-        publish_date: hash['publish_date'],
-        cover_state: hash['cover_state'],
-        archived: hash['archived']
+        class: hash['class'], id: hash['id'],
+        publisher: hash['publisher'], publish_date: hash['publish_date'],
+        cover_state: hash['cover_state'], archived: hash['archived']
       }
-      actual_id = hash['id']
 
       expect(actual_items).to eq expected_items
-      expect(actual_id).to be expected_id
     end
 
     it 'return correct hash for a Book with label' do
       require './lib/label'
-
       the_label = Label.new('the lable', 'the color')
       the_book = Book.new(publisher, cover_state, publish_date)
-
       the_book.label = the_label
+
       hash = the_book.to_hash
 
       expected_label = the_label.id
