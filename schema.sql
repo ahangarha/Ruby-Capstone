c3c002fd422c15d867609f5c8f8d45ca5aa9bc5b
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-24 13:02:59 +0300

    add schema for books

--- schema.sql
+++ schema.sql
@@ -4,3 +4,13 @@ CREATE TABLE labels(
     color VARCHAR(255),
     PRIMARY KEY (id)
 );
+
+CREATE TABLE books(
+    id INT GENERATED ALWAYS AS IDENTITY,
+    publish_data DATE,
+    archived BOOLEAN,
+    publisher VARCHAR(255),
+    cover_state VARCHAR(255),
+    label_id INT,
+    FOREIGN KEY (label_id) REFERENCES labels(id)
+);
