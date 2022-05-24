d41709643f1203fc68c82b4864a8714dceccc55f
Mostafa Ahangarha <ahangarha@riseup.net>
2022-05-24 20:40:02 +0300

    implement to_hash for Book class

--- lib/book.rb
+++ lib/book.rb
@@ -9,6 +9,21 @@ class Book < Item
     @cover_state = cover_state
   end
 
+  def to_hash
+    hash = {
+      'class' => 'Book',
+      'id' => @id,
+      'publisher' => @publisher,
+      'publish_date' => @publish_date,
+      'cover_state' => @cover_state,
+      'archived' => @archived
+    }
+
+    hash['label'] = @label.id unless @label.nil?
+
+    hash
+  end
+
   private
 
   def can_be_archived?
