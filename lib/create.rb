e0caa5b70f7112b2a711a4de1f055f28b0d638fa
Bavon101 <bavonfate@gmail.com>
2022-05-25 11:54:06 +0300

    Add a method to add an music item and all of it's accessors

--- lib/create.rb
+++ lib/create.rb
@@ -22,3 +22,31 @@ class CreateBook
     Book.new(publisher, cover_state, publish_date)
   end
 end
+
+class CreateMusicAlbum < Create
+  require_relative './label'
+  require_relative './genre'
+  require_relative './music_album'
+
+  def self.create
+    print 'title: '
+    title = gets.chomp.strip
+    print 'label-color: '
+    color = gets.chomp.strip
+    label = Label.new title, color
+    print 'genre: '
+    genre_name = gets.chomp.strip
+    genre = Genre.new genre_name
+    print 'Publish Date: '
+    publish_date = gets.chomp.strip
+    album = MusicAlbum.new publish_date
+    album.genre = genre
+    album.label = label
+    puts "#{'-' * 20}Book has been created 🎉#{'-' * 20}"
+    {
+      genre: genre,
+      album: album,
+      label: label
+    }
+  end
+end
