728da728ee372d88de081b545d10fd9c7ad44598
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-05-26 14:55:36 +0530

    Added more tests for game class

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,4 +13,15 @@ describe Game do
       expect(game.multiplayer).to be false
     end
   end
+  context 'can_be_archived? method' do
+    it 'returns true if parent return true' do
+      multiplayer = false
+      last_played_at = 2021
+      publish_date = 2020
+
+      the_game = Game.new(multiplayer, last_played_at, publish_date)
+      actual = the_game.send(:can_be_archived?)
+      expect(actual).to be_falsey
+    end
+  end
 end
